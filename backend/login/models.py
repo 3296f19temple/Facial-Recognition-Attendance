58786493fbfe4b219f07bdb8b09f96ea40cc2f36
Chris Bechter <chris.bechter@temple.edu>
2019-11-24 00:45:21 +0000

    added students and attendances

--- backend/login/models.py
+++ backend/login/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from djongo import models
+import datetime
+
 
 class Classes(models.Model):
     courseId = models.IntegerField()
     courseName = models.TextField()
     meetingSchedule = models.TextField()
+
+class Attendances(models.Model):
+    attendanceTaken = models.BooleanField()
+    attendanceTime = models.DateTimeField(default = datetime.datetime.now)
+
+class Students(models.Model):
+    studentID = models.IntegerField()
+    studentName = models.TextField()
+    studentPicture = models.FileField(blank=False, null=False)
+
+    attendance = models.EmbeddedModelField(
+        model_container = Attendances
+        )
