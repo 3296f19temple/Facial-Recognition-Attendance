18074b2a0be213dbee48b4b432b45f0f0d78bf11
kyparisisg <tuk12920@temple.edu>
2019-11-24 00:58:57 +0000

    foreign keys Tyler

--- backend/login/models.py
+++ backend/login/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Classes(models.Model):
     courseId = models.IntegerField()
     courseName = models.TextField()
     meetingSchedule = models.TextField()
+    username  =  models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return '%d' %(self.courseId)
+
+class Students(models.Model):
+    courseId = models.ForeignKey(Classes, on_delete=models.CASCADE)
+    first_name = models.TextField()
+    last_name = models.TextField()
+    school_id = models.CharField(max_length = 200)
+    attendence = models.CharField(max_length = 800)
+
+    def __str__(self):
+       return '%d' %(self.courseId)
+
